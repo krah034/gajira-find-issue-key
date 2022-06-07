f3a3113a4bd203e1c33593edd00fe7f2c2da8cec
Benjamin Krahl <23114746+krah034@users.noreply.github.com>
2022-06-07 13:40:08 +0200

    Use bearer token to login to Jira Server

--- common/net/Jira.js
+++ common/net/Jira.js
@@ -74,7 +74,7 @@ class Jira {
     }
 
     if (headers.Authorization === undefined) {
-      headers.Authorization = `Basic ${Buffer.from(`${this.email}:${this.token}`).toString('base64')}`
+      headers.Authorization = `Bearer ${Buffer.from(`${this.token}`)}`
     }
 
     // strong check for undefined
